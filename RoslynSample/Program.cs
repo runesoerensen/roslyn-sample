9513d8d5e2a2cd816850c2e8d063d285bc1355a6
Rune Soerensen <rs@appharbor.com>
2015-08-03 15:14:09 -0700

    Add some test output

--- RoslynSample/Program.cs
+++ RoslynSample/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace RoslynSample
+﻿using System;
+using System.Threading;
+
+namespace RoslynSample
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
+			Console.WriteLine("Hello world");
+			Thread.Sleep(5000);
+
+			Console.WriteLine("Bye");
+			Thread.Sleep(1000);
 		}
 	}
 }
