47ffb1f3734094b3863f7bfe58f1aa86c969b288
Rune Soerensen <rs@appharbor.com>
2015-08-05 14:49:55 -0700

    Add default VS2015 MVC 5 template

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoslynSample.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return Content($"Hello there. The clock is {DateTime.UtcNow}");
+		}
+	}
+}
---
+++ Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoslynSample.Web.MvcApplication" Language="C#" %>
